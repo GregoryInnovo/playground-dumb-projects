d823e79e3aa4fced3948d0b2004e6246545b89d0
Gregson Murcia <56243758+GregoryInnovo@users.noreply.github.com>
2025-05-20 14:31:45 -0500

    [GM] [ADD] point collectable script

---
+++ Assets/Scripts/PointCollectible.cs
@@ -0,0 +1,17 @@
+// using UnityEngine;
+
+// public class PointCollectible : MonoBehaviour
+// {
+//     void OnTriggerEnter(Collider other)
+//     {
+//         if (other.CompareTag("Player"))
+//         {
+//             PlayerController player = other.GetComponent<PlayerController>();
+//             if (player != null)
+//             {
+//                 player.AddPoint();
+//             }
+//             Destroy(gameObject);
+//         }
+//     }
+// }
